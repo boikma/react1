5596205260c264618dfc5256772ac1d20aa85c2e
boicech <boikma9@gmail.com>
2020-06-10 09:45:17 +0700

    up lan dau

---
+++ precache-manifest.438ad776826989d05e1a9458c0ce8693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4519e4d7f20faa22c4845f0fd7f0dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7772683e1dc4d8b633",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57c54279514b407f69ed",
+    "url": "/static/js/2.117249e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.117249e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7772683e1dc4d8b633",
+    "url": "/static/js/main.bdd8872e.chunk.js"
+  },
+  {
+    "revision": "e8517b0af8232c5d3d7c",
+    "url": "/static/js/runtime-main.800edfd7.js"
+  }
+]);
